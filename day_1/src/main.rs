f9b7262cbcc601fa40f0867a100ce689dbec4e7f
Smaug123 <patrick+github@patrickstevens.co.uk>
2021-05-08 11:37:17 +0100

    Rejig to an actual workspace

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -38,6 +38,12 @@ pub fn part_2(numbers: &[u32]) -> u32 {
     sum
 }
 
+fn main() {
+    let input = input();
+    println!("part 1 => {}", part_1(&input));
+    println!("part 2 => {}", part_2(&input));
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
