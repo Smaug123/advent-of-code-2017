30513720d48fccc14513151bff530b444f256864
Smaug123 <patrick+github@patrickstevens.co.uk>
2021-05-12 12:22:52 +0100

    Fix it all up

--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -7,10 +7,15 @@ struct Node<'a> {
     children: Vec<&'a str>,
 }
 
-struct Tree<'a> {
-    children: Vec<&'a Tree<'a>>,
+struct TreeNode<'a> {
+    children: Vec<usize>,
     name: &'a str,
     weight: u32,
+}
+
+struct Tree<'a> {
+    nodes: Vec<TreeNode<'a>>,
+    root: usize,
 }
 
 enum ParserState {
@@ -105,63 +110,145 @@ fn input() -> Vec<Node<'static>> {
     input.lines().map(|l| parse_line(l)).collect::<Vec<Node>>()
 }
 
-fn tree_it<'a>(nodes: &[Node<'a>]) -> (Vec<Tree<'a>>, usize) {
+fn tree_it<'a>(nodes: &[Node<'a>]) -> Tree<'a> {
     let nodes_by_name: HashMap<&'a str, (&Node<'a>, usize)> = nodes
         .iter()
         .enumerate()
         .map(|(count, i)| (i.name, (i, count)))
         .collect();
-    let mut built: Vec<Tree<'a>> = Vec::with_capacity(nodes.len());
+    let mut built: Vec<TreeNode<'a>> = nodes
+        .iter()
+        .map(|n| TreeNode {
+            name: n.name,
+            weight: n.weight,
+            children: vec![],
+        })
+        .collect();
     let mut is_set: Vec<bool> = vec![false; nodes.len()];
 
     let mut stack: Vec<usize> = vec![];
-    let mut top_node = None;
     for parent_node_index in 0..nodes.len() {
         if !is_set[parent_node_index] {
             stack.push(parent_node_index);
             while let Some(node_index) = stack.pop() {
+                if is_set[node_index] {
+                    continue;
+                }
                 let node = &nodes[node_index];
-                let (existing, required): (Vec<_>, Vec<_>) = (*node)
-                    .children
-                    .iter()
-                    .map(|&i| {
-                        let (_, child_index) = nodes_by_name.get(i).unwrap();
-                        (i, *child_index)
-                    })
-                    .partition(|&(_, b)| is_set[b]);
-                let required = required.iter().map(|&(name, _)| {
-                    let (_, child_index) = nodes_by_name.get(name).unwrap();
+                let children = (*node).children.iter().map(|&i| {
+                    let (_, child_index) = nodes_by_name.get(i).unwrap();
                     *child_index
                 });
-                let old_len = stack.len();
-                stack.extend(required);
-                if stack.len() == old_len {
-                    built[node_index] = Tree {
-                        name: node.name,
-                        weight: node.weight,
-                        children: vec![],
-                    };
-                    built[node_index]
-                        .children
-                        .extend(existing.iter().map(|&(_, value)| &built[value]));
-                    is_set[node_index] = true;
-                } else {
-                    top_node = Some(node_index);
-                }
+                built[node_index].children.extend(children);
+                stack.extend(built[node_index].children.iter());
+                is_set[node_index] = true;
             }
         }
     }
-    (built, top_node.unwrap())
+
+    // We'll reuse the is_set array for an "is possibly the root" array.
+    // They're all true now.
+    for node in built.iter() {
+        for child in node.children.iter() {
+            is_set[*child] = false;
+        }
+    }
+    let (top_node, _) = is_set.iter().enumerate().find(|(_, v)| **v).unwrap();
+    Tree {
+        nodes: built,
+        root: top_node,
+    }
 }
 
 fn part_1<'a>(nodes: &[Node<'a>]) -> &'a str {
-    let (trees, top_node) = tree_it(nodes);
-    trees[top_node].name
+    let tree = tree_it(nodes);
+    tree.nodes[tree.root].name
 }
 
-fn part_2(nodes: &[Node]) -> u32 {
+#[derive(Debug)]
+struct BalancedWeight {
+    child_weights: u32,
+    self_weight: u32,
+    child_count: usize,
+}
+
+fn total_weight(b: &BalancedWeight) -> u32 {
+    b.child_weights * (b.child_count as u32) + b.self_weight
+}
+
+fn weight(tree: &Tree, node: usize) -> Result<BalancedWeight, i32> {
+    let node = &tree.nodes[node];
+    let children: Vec<_> = node
+        .children
+        .iter()
+        .map(|node| weight(&tree, *node))
+        .collect();
+
+    let count = children.len();
+    let mut c = children.iter();
+    let mut different_child = None;
+    if let Some(child) = c.next() {
+        match child {
+            Err(e) => Err(*e),
+            Ok(child) => {
+                for other in c {
+                    match other {
+                        Err(e) => return Err(*e),
+                        Ok(other) => {
+                            if total_weight(other) != total_weight(child) {
+                                match different_child {
+                                    None => {
+                                        different_child = Some(other);
+                                    }
+                                    Some(different_child) => {
+                                        if total_weight(other) == total_weight(different_child) {
+                                            // The bad one is `child`.
+                                            return Err(total_weight(other) as i32
+                                                - (child.child_weights * child.child_count as u32)
+                                                    as i32);
+                                        } else {
+                                            // The bad one is `other`.
+                                            return Err(total_weight(child) as i32
+                                                - (other.child_weights * other.child_count as u32)
+                                                    as i32);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                match different_child {
+                    None => Ok(BalancedWeight {
+                        self_weight: node.weight,
+                        child_weights: total_weight(child),
+                        child_count: count,
+                    }),
+                    Some(different_child) => {
+                        // The bad one is `different_child`.
+                        Err(total_weight(child) as i32
+                            - (different_child.child_weights * different_child.child_count as u32)
+                                as i32)
+                    }
+                }
+            }
+        }
+    } else {
+        Ok(BalancedWeight {
+            self_weight: node.weight,
+            child_weights: 0,
+            child_count: 0,
+        })
+    }
+}
+
+fn part_2(nodes: &[Node]) -> i32 {
     let tree = tree_it(nodes);
-    0
+    let result = weight(&tree, tree.root);
+    match result {
+        Err(e) => e,
+        Ok(w) => panic!("Expected unbalanced tree, got {:?}", w),
+    }
 }
 
 fn main() {
