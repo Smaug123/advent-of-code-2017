0dcca4db86e3aeecc9f957f79e89105812b519e6
Smaug123 <patrick+github@patrickstevens.co.uk>
2021-05-15 10:05:36 +0100

    Day 3 benchmarks

--- day_3/benches/day_3.rs
+++ day_3/benches/day_3.rs
@@ -1,12 +1,15 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use day_5::day_5::{input, part_2};
+use day_3::day_3::{part_1, part_2};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("day 5 part 2", |b| {
-        let input = input();
+    c.bench_function("day 3 part 1", |b| {
         b.iter(|| {
-            let mut input = input.to_vec();
-            part_2(&mut input);
+            part_1(500000);
+        })
+    });
+    c.bench_function("day 3 part 2", |b| {
+        b.iter(|| {
+            part_2(500000);
         })
     });
 }
